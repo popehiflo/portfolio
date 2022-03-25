3f2ee97311261ff4cd269674c5a38e2c52c80e1a
popehiflo <poolpetterhijuel@hotmail.com>
2022-03-25 13:31:19 -0500

    feat: config .env, script nodemon

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
@@ -6,6 +8,8 @@ app.get('/', (req, res) => {
   res.json('Hola, respondo a GET /');
 });
 
-app.listen(3000, () => {
-  console.log('Server corriendo...');
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
