6b13efbdd994ddf8f094d6d82fc9bcf90e4b029e
popehiflo <poolpetterhijuel@hotmail.com>
2022-03-25 13:40:22 -0500

    feat: install morgan y habilita lectura del body

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(express.json);
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.json('Hola, respondo a GET /');
