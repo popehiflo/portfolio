0b3ef6044b55c49e8b8b301ee8b25f60f2b21977
popehiflo <poolpetterhijuel@hotmail.com>
2022-03-25 12:54:54 -0500

    feat: install express y test de server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('soy el entry point');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json('Hola, respondo a GET /');
+});
+
+app.listen(3000, () => {
+  console.log('Server corriendo...');
+});
