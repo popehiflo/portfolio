2ea594e7dce7c24dfcb1f5da5512fb9437798750
popehiflo <poolpetterhijuel@hotmail.com>
2022-03-26 17:43:00 -0500

    feat: routes y endpoints

--- api/user/index.js
+++ api/user/index.js
@@ -1,8 +1,19 @@
 const { Router } = require('express');
-const { handlerAllUser } = require('./user.controller');
+
+const { 
+  handlerAllUser,
+  handlerOneUser,
+  handlerCreateUser,
+  handlerUpdateUser,
+  handlerDeleteUser
+} = require('./user.controller');
 
 const router = Router();
 
 router.get('/', handlerAllUser);
+router.get('/:id', handlerOneUser);
+router.post('/', handlerCreateUser);
+router.put('/:id', handlerUpdateUser);
+router.delete('/:id', handlerDeleteUser);
 
 module.exports = router;
