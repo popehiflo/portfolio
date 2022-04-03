f672e1b1affc1cc78195c2dffad883c25b949d41
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-03 16:13:27 -0500

    feat: Auth, sign In User

--- auth/auth.service.js
+++ auth/auth.service.js
@@ -14,6 +14,26 @@ async function signUpUser(user) {
   return newUser;
 }
 
+async function signInUser(user) { 
+  const { email, password } = user;
+
+  try {
+    const existUser = await UserModel.findOne({ email });
+
+    if (!existUser) {
+      return null;
+    }
+    const decryptedPassword = CryptoJS.AES.decrypt(existUser.password, process.env.PASS_SECRET_KEY).toString(CryptoJS.enc.Utf8);
+    if (decryptedPassword !== password) {
+      return null;
+    }
+    return existUser;
+  } catch (error) {
+    return null;
+  }
+}
+
 module.exports = {
-  signUpUser
+  signUpUser,
+  signInUser,
 }
