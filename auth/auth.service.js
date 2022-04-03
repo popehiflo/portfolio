a8cc2e9b7ba1a1e615f23eb1aff92ce4a9a56762
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-03 15:18:20 -0500

    feat: Auth, encrypt password User

--- auth/auth.service.js
+++ auth/auth.service.js
@@ -1,13 +1,16 @@
 const UserModel = require("../api/user/user.model");
+const CryptoJS = require("crypto-js");
 
 async function signUpUser(user) {
-  const { email } = user;
+  const { email, password } = user;
   const existEmail = await UserModel.findOne({ email });
 
   if (existEmail) {
     return null;
   }
-  const newUser = await UserModel.create(user);
+  const newUser = new UserModel(user);
+  newUser.password = CryptoJS.AES.encrypt(password, process.env.PASS_SECRET_KEY).toString();
+  await newUser.save();
   return newUser;
 }
 
