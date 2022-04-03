127e89dc16717c5fc33f8bca92861b8fd21d4b58
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-03 14:45:28 -0500

    feat: declaration API routes

--- routes.js
+++ routes.js
@@ -3,14 +3,20 @@
  */
 const authLocal = require('./auth/local');
 const user = require('./api/user');
+const cart = require('./api/cart');
+const order = require('./api/order');
+const service = require('./api/service');
 const speciality = require('./api/speciality');
 
 function routes(app) {
-  // auth Routes
+  // Auth Routes
   app.use('/auth/local', authLocal);
 
   // API Routes
   app.use('/api/users', user);
+  app.use('/api/carts', cart);
+  app.use('/api/orders', order);
+  app.use('/api/services', service);
   app.use('/api/specialities', speciality);
 }
 
