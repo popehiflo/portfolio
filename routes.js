cb7a6ec69d92530965a0c76e040b0966776a6c6f
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-03 09:37:11 -0500

    feat: routes

--- routes.js
+++ routes.js
@@ -1,11 +1,17 @@
 /**
  * Main application routes
  */
+const authLocal = require('./auth/local');
 const user = require('./api/user');
+const speciality = require('./api/speciality');
 
 function routes(app) {
+  // auth Routes
+  app.use('/auth/local', authLocal);
+
   // API Routes
   app.use('/api/users', user);
+  app.use('/api/specialities', speciality);
 }
 
 module.exports = routes;
